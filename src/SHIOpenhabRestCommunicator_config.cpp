e9fdbeceae463d23d40a8100729276cac408bbf2
Karsten Becker <kb@karsten-becker.de>
2020-03-25 20:32:02 +0100

    Updated config

--- src/SHIOpenhabRestCommunicator_config.cpp
+++ src/SHIOpenhabRestCommunicator_config.cpp
@@ -6,7 +6,7 @@
 
 // WARNING, this is an automatically generated file!
 // Don't change anything in here.
-// Last update 2020-03-15
+// Last update 2020-03-25
 
 # include <iostream>
 # include <string>
@@ -15,8 +15,12 @@
 # include "SHIOpenhabRestCommunicator.h"
 // Configuration implementation for class SHI::OpenhabRestCommunicatorConfig
 
+namespace {
+    
+}  // namespace
+
 SHI::OpenhabRestCommunicatorConfig::OpenhabRestCommunicatorConfig(const JsonObject &obj):
-       CONNECT_TIMEOUT(obj["CONNECT_TIMEOUT"] | 500),
+      CONNECT_TIMEOUT(obj["CONNECT_TIMEOUT"] | 500),
       DATA_TIMEOUT(obj["DATA_TIMEOUT"] | 1000),
       hierachySeperator(obj["hierachySeperator"] | "_"),
       baseURL(obj["baseURL"] | "http://192.168.188.250:8080"),
